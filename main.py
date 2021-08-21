81ee125a65bd323224c3047e2cfb69075d7b4996
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-21 08:46:52 -0700

    created summary csv of number of squirrels from 2018 squirrel census

--- main.py
+++ main.py
@@ -9,7 +9,30 @@
 #     print(temperatures)
 
 
+# import pandas
+#
+# data = pandas.read_csv('weather_data.csv')
+# monday = (data[data.day == 'Monday'])
+#
+#
+# def farenheight_converter(celsius):
+#     return ((celsius * (9/5)) + 32)
+#
+# print(farenheight_converter(monday.temp))
 import pandas
 
-data = pandas.read_csv('weather_data.csv')
-print(data)
+data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+
+grey_count = len(data[data['Primary Fur Color'] == 'Gray'])
+red_count = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+black_count = len(data[data['Primary Fur Color'] == 'Black'])
+print(grey_count)
+print(red_count)
+print(black_count)
+
+squirrel_dict = {
+    "Fur Color" : ['gray', 'red', 'black'],
+    'Number' : [grey_count, red_count, black_count]
+}
+squirrel_summary = pandas.DataFrame(squirrel_dict)
+squirrel_summary.to_csv('squrrel_summary.csv')
